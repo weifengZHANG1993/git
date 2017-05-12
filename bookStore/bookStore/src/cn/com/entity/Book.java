4f12a51ade7b5f9f82227b9270b073fe2901e03f
zhangweifengzkr <930341169@qq.com>
2017-05-12 17:04:09 +0800

    没有上传的文本

--- bookStore/bookStore/src/cn/com/entity/Book.java
+++ bookStore/bookStore/src/cn/com/entity/Book.java
@@ -5,10 +5,3 @@ public class Book {
 	private String id;
 	private Integer price;
 }
-package cn.com.entity;
-
-public class Book {
-	private String name;
-	private String id;
-	private Integer price;
-}
